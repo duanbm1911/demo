0a0fe2311633df199a8d01ec2b2bc5d57c322290
User <user@example.com>
2026-01-30 11:24:58 +0000

    Add demo-kr669u7e.b4a.run to ALLOWED_HOSTS

--- webapp/settings.py
+++ webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*3l#o64@5mjm#f&_9l-2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,demo-kr669u7e.b4a.run').split(',')
 
 
 # Application definition
